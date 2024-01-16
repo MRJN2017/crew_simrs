3bd9ab1c0e2571577ec76de53c257cbe783a4643
Nino_Tralala <105920067+MRJN2017@users.noreply.github.com>
2024-01-16 21:41:57 +0800

    menampilkan list user berdasarkan level

--- application/views/list_job.php
+++ application/views/list_job.php
@@ -3,38 +3,43 @@
 
 </div>
 <div class="jumbotron bg-dark text-white">
-    <table class="table table-light">
-        <thead>
-            <tr>
-                <th scope="col">No</th>
-                <th scope="col">Tanggal</th>
-                <th scope="col">Unit Pelapor</th>
-                <th scope="col">Nama Pelapor</th>
-                <th scope="col">List Kerjaan</th>
-                <th scope="col">Status</th>
-                <th scope="col">Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($listUserById as $i => $lj) : ?>
+    <a class="btn btn-primary" href="<?= base_url('patroli/excel_hasil_patroli') ?>" target="_blank" rel="noopener">
+        <i class="fa fa-file-excel-o"></i> Export data pelaporan
+    </a><br><br>
+    <div class="table-responsive">
+
+        <table class="table table-bordered table-striped table-light">
+            <thead>
                 <tr>
-                    <td><?= ($i + 1) ?></td>
-                    <td><?= $lj->tanggal ?></td>
-                    <td><?= $lj->nama_divisi ?></td>
-                    <td><?= $lj->nama_pelapor ?></td>
-                    <td><?= $lj->tugas ?></td>
-                    <td><?= $lj->status ?></td>
-                    <td>
-                        <!-- <a href="<?= base_url('jobdesk/list_jobById/' . $ls->id_user) ?>" class="btn btn-primary btn-sm"><i class="fa fa-search"></i> Detail Kerjaan</a> -->
-
-                        <a href="<?= base_url('') ?>" class="btn btn-success btn-sm  ml-2"><i class=" fa fa-pencil-square-o "></i> Edit</a>
-
-                        <a href=" <?= base_url('') ?>" class="btn btn-primary btn-sm btn-delete ml-2" onclick="return false"><i class="fa fa-check"></i> Selesai</a>
-                    </td>
+                    <th scope="col">No</th>
+                    <th scope="col">Tanggal</th>
+                    <th scope="col">Unit Pelapor</th>
+                    <th scope="col">Nama Pelapor</th>
+                    <th scope="col">List Kerjaan</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Aksi</th>
                 </tr>
-            <?php endforeach; ?>
+            </thead>
+            <tbody>
+                <?php foreach ($listUserById as $i => $lj) : ?>
+                    <tr>
+                        <td><?= ($i + 1) ?></td>
+                        <td><?= $lj->tanggal ?></td>
+                        <td><?= $lj->nama_divisi ?></td>
+                        <td><?= $lj->nama_pelapor ?></td>
+                        <td><?= $lj->tugas ?></td>
+                        <td><?= $lj->status ?></td>
+                        <td>
+                            <!-- <a href="<?= base_url('jobdesk/list_jobById/' . $ls->id_user) ?>" class="btn btn-primary btn-sm"><i class="fa fa-search"></i> Detail Kerjaan</a> -->
 
-            
-        </tbody>
-    </table>
-</div>
+                            <a href="<?= base_url('') ?>" class="btn btn-success btn-sm  ml-2"><i class=" fa fa-pencil-square-o "></i> Edit</a>
+
+                            <a href=" <?= base_url('') ?>" class="btn btn-primary btn-sm btn-delete ml-2" onclick="return false"><i class="fa fa-check"></i> Selesai</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+
+
+            </tbody>
+        </table>
+    </div>
--- application/views/list_user.php
+++ application/views/list_user.php
@@ -1,31 +1,11 @@
 <div class="row">
-    <h2 class="mb-4">List Karyawan</h2>
-
+    <h2 class="mb-4">List Kerjaan</h2>
 </div>
-<div class="d-inline ml-auto float-left" style="margin-top: 10px; margin-left: 10px;">
-    <!-- <span class="text-white"> => Pilih nama anda pada list dibawah ini dan Silakan menambahkan Kerjaan baru anda</span> -->
-
-
-    <!-- <div class="d-inline ml-auto float-left">
-        <a href="#" class="btn btn-success btn-sm btn-add-divisi" data-toggle="modal" data-target="#modal-add-divisi"><i class="fa fa-plus"></i> Tambah Kerjaan</a>
-    </div> -->
-    <br>
-</div>
-<!-- <?php if (is_level('captain')) : ?>
-    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-        Tambah Anggota
-    </button>
-    <div class="d-inline ml-auto float-left" <?= @$_active ?>>
-        <br>
-        <a href="#" class="btn btn-success btn-sm btn-add-divisi" data-toggle="modal" data-target="#modal-add-divisi"><i class="fa fa-plus"></i> Tambah Kerjaan untuk crew</a>
-    </div>
-<?php endif; ?> -->
-
 <div class="jumbotron bg-dark text-white">
     <br>
     <br>
-    <table class="table table-light">
-        <thead>
+    <table class="table table-bordered table-striped table-light">
+        <thead class="thead-light">
             <tr>
                 <th scope="col">No</th>
                 <th scope="col">Karyawan</th>
@@ -33,67 +13,28 @@
             </tr>
         </thead>
         <tbody>
-            <?php foreach ($list_user as $i => $ls) : ?>
+            <?php if (is_level('captain')) : ?>
+                <?php foreach ($list_user as $i => $ls) : ?>
+                    <tr>
+                        <td><?= ($i + 1) ?></td>
+                        <td><?= $ls->nama ?></td>
+                        <td>
+                            <a href="<?= base_url('jobdesk/list_jobById/' . $ls->id_user) ?>" class="btn btn-primary btn-sm"><i class="fa fa-search"></i> Detail Kerjaan</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <!-- Tampilkan hanya user yang sedang login -->
+                <?php
+                ?>
                 <tr>
-                    <td><?= ($i + 1) ?></td>
-                    <td><?= $ls->nama ?></td>
+                    <td>1</td>
+                    <td><?= $this->session->nama ?></td>
                     <td>
-                        <a href="<?= base_url('jobdesk/list_jobById/' . $ls->id_user) ?>" class="btn btn-primary btn-sm"><i class="fa fa-search"></i> Detail Kerjaan</a>
+                        <a href="<?= base_url('jobdesk/list_jobById/' . $this->session->id_user) ?>" class="btn btn-primary btn-sm"><i class="fa fa-search"></i> Detail Kerjaan</a>
                     </td>
                 </tr>
-            <?php endforeach; ?>
+            <?php endif; ?>
         </tbody>
     </table>
-</div>
-<!-- <div class="modal fade" id="modal-add-divisi" tabindex="-1" role="dialog" aria-labelledby="modal-add-divisi-label" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content bg-light">
-            <form id="form-add-divisi" action="<?= base_url('jobdesk/store') ?>" method="post" onsubmit="return false">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="modal-add-divisi-label">Tambah Kerjaan Baru</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body bg-dark">
-                    <div class="form-group">
-                        <label for="nama-divisi" class="text-white">Kerjaan :</label>
-                        <input type="text" name="nama_divisi" id="nama-divisi" class="form-control" placeholder="Nama Divisi" required="reuired" />
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div> -->
-
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="<?= base_url('jobdesk/store') ?>" method="post">
-                    <div class="form-group">
-                        <!-- <label for="kerja">Masukan Kerjaan :</label> -->
-                        <input type="text" name="kerja" id="kerja" class="form-control" placeholder="Nama Kerjaan" required="reuired" />
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                    </div>
-                </form>
-            </div>
-
-
-        </div>
-    </div>
 </div>
